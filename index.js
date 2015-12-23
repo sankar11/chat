fd66f66ebcf5a8e31569ffd4354873d616ba53ec
sankar <sesankar11@gmail.com>
2015-12-23 23:12:36 +0530

    simple hello world

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+  console.log('Example app listening at http://%s:%s', host, port);
 });
